48d94dd9b4b56d52a5c4fc56b345df8c3f26588a
HeavyXenon <medaka1cocoa1@gmail.com>
2025-06-24 22:22:35 +0900

    Initialize build system for OpenGLApp with CMake
    - Created necessary build files including compiler dependency, flags, and include response files.
    - Configured linking with required libraries such as GLFW and OpenGL.
    - Added object files for source code compilation.
    - Set up installation script and compile commands for the project.
    - Generated Makefile for build automation.

---
+++ build/CMakeFiles/OpenGLApp.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/rkowp/Documents/GitHub/GenesisForge/src/glad.c" "CMakeFiles/OpenGLApp.dir/src/glad.c.obj" "gcc" "CMakeFiles/OpenGLApp.dir/src/glad.c.obj.d"
+  "C:/Users/rkowp/Documents/GitHub/GenesisForge/src/application.cpp" "CMakeFiles/OpenGLApp.dir/src/application.cpp.obj" "gcc" "CMakeFiles/OpenGLApp.dir/src/application.cpp.obj.d"
+  "C:/Users/rkowp/Documents/GitHub/GenesisForge/src/camera.cpp" "CMakeFiles/OpenGLApp.dir/src/camera.cpp.obj" "gcc" "CMakeFiles/OpenGLApp.dir/src/camera.cpp.obj.d"
+  "C:/Users/rkowp/Documents/GitHub/GenesisForge/src/fullscreen_manager.cpp" "CMakeFiles/OpenGLApp.dir/src/fullscreen_manager.cpp.obj" "gcc" "CMakeFiles/OpenGLApp.dir/src/fullscreen_manager.cpp.obj.d"
+  "C:/Users/rkowp/Documents/GitHub/GenesisForge/src/main.cpp" "CMakeFiles/OpenGLApp.dir/src/main.cpp.obj" "gcc" "CMakeFiles/OpenGLApp.dir/src/main.cpp.obj.d"
+  "C:/Users/rkowp/Documents/GitHub/GenesisForge/src/opengl_utils.cpp" "CMakeFiles/OpenGLApp.dir/src/opengl_utils.cpp.obj" "gcc" "CMakeFiles/OpenGLApp.dir/src/opengl_utils.cpp.obj.d"
+  "C:/Users/rkowp/Documents/GitHub/GenesisForge/src/timer.cpp" "CMakeFiles/OpenGLApp.dir/src/timer.cpp.obj" "gcc" "CMakeFiles/OpenGLApp.dir/src/timer.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
